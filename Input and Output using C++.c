040bdb3ffa77f8059077a27261c626866670c614
muskantomar990 <68536462+muskantomar990@users.noreply.github.com>
2020-10-22 03:08:30 -0700

    Add files via upload

---
+++ Input and Output using C++.c
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c;
+    int sum;
+    cout<<"";
+    cin>>a;
+    cout<<"";
+    cin>>b;
+    cout<<"";
+    cin>>c;
+    sum=a+b+c;
+    cout<<""<<sum<<endl;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
